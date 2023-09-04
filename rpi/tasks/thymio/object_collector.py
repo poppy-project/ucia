e36504203bc44f1e1bf697ea4bb67e407efcc552
Etienne SCHMITZ <contact@etienne-schmitz.com>
2023-09-04 11:34:07 +0100

    feat - Finish to complete vision
    - Add requirements.txt
    - Create camera class
    - Put some code in object collector example

--- rpi/tasks/thymio/object_collector.py
+++ rpi/tasks/thymio/object_collector.py
@@ -1,18 +1,36 @@
 import logging
+import cv2 as cv
 
 from controller.base import BaseController
+from vision.camera import Camera
 from tasks.base import Task
+
+from vision import detect_objects
+
 
 class ObjectCollector(Task):
     def __init__(self, controller: BaseController):
         self.controller = controller
         self.logger = logging.getLogger(__name__)
+        self.cam = Camera()
 
     def initialize(self):
         pass
 
     def run(self):
         self.logger.info("OBJECT COLLECTOR")
+        img = self.cam.grab_frame_loop()    
+
+        if img is None:
+            return
+
+        found_obj = detect_objects(img, render=True)
+        print(found_obj)
+        # cv.imshow('Object Detection', img)
+        # cv.waitKey(1)
+
+        
+    
 
     def close(self):
         pass
--- rpi/vision/camera.py
+++ rpi/vision/camera.py
@@ -1,2 +1,10 @@
+import cv2 as cv
+
 class Camera:
-    pass
+    def __init__(self):
+        self.cap = cv.VideoCapture(0)
+
+    def grab_frame_loop(self):
+        _, img = self.cap.read()
+
+        return img
