f7de8b940f6915e018a47c4afbd21d2478339cea
Etienne Schmitz <contact@etienne-schmitz.com>
2023-10-28 15:13:47 +0200

    fix - resolve some errors with tailwind integration
    Add watchdog for develop experience in flask
    Remove purge in config (upgrade)

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    purge: [],
     darkMode: true,
-    content: ["./app/templates/**/*.{html,htm}"],
+    content: [
+        "./app/templates/**/*.{html,htm}"
+    ],
     theme: {
         extend: {},
     },
