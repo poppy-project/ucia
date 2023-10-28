c955b5ca7118ef00ba842f1db8ef0541d8b9721f
Etienne Schmitz <contact@etienne-schmitz.com>
2023-10-28 13:32:53 +0200

    feat - add all routes

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,7 +1,53 @@
 from flask import Blueprint, render_template
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
-@main.route('/')
+
+@main.context_processor
+def inject_robot_config():
+    return dict(version="0.1")
+
+
+@main.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
+@main.route("/docs")
+def docs():
+    return render_template("modules/docs.html")
+
+
+@main.route("/logs")
+def logs():
+    return render_template("modules/logs.html")
+
+
+@main.route("/program")
+def program():
+    return render_template("modules/program.html")
+
+
+@main.route("/settings")
+def settings():
+    return render_template("modules/settings.html")
+
+
+@main.route("/terminal")
+def terminal():
+    return render_template("modules/terminal.html")
+
+
+@main.route("/webcam")
+def webcam():
+    return render_template("modules/webcam.html")
+
+
+@main.route("/shutdown")
+def shutdown():
+    return None
+
+
+@main.route("/reboot")
+def reboot():
+    return None
