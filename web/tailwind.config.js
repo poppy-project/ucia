b6618c46d3a5d9e7215aaa3e529a0157a26fc8fd
Etienne Schmitz <contact@etienne-schmitz.com>
2023-10-28 15:02:46 +0200

    feat - add tailwind

---
+++ web/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    purge: [],
+    darkMode: true,
+    content: ["./app/templates/**/*.{html,htm}"],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
