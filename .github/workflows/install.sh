c817535c11a41f88957426caee5dce9b93b291fb
Etienne Schmitz <contact@etienne-schmitz.com>
2023-12-03 01:37:34 +0100

    feat - update install.sh with the new rasbian

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-ls
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
-sed -i 's/stable\/updates/stable-security\/updates/' /etc/apt/sources.list
+echo "deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi" | sudo tee /etc/apt/sources.list
+
+sudo apt update
+
+echo "DEBUG"
+
+# sed -i 's/stable\/updates/stable-security\/updates/' /etc/apt/sources.list
+# deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
