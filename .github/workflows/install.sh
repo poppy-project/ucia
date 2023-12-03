b33e494f0f0960fc27fec71dc5d9ebf7322c8a49
Etienne Schmitz <contact@etienne-schmitz.com>
2023-12-03 01:42:29 +0100

    fix - remove sudo

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
 echo "deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi" | sudo tee /etc/apt/sources.list
 
-sudo apt update
-
-echo "DEBUG"
+apt update
 
 # sed -i 's/stable\/updates/stable-security\/updates/' /etc/apt/sources.list
 # deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
