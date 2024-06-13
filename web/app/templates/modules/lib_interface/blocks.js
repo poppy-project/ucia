2e48edd3ba6ade54cd465aca2ae2e136fc3b45ba
Manon-Arc <arcasmanon47@gmail.com>
2024-06-13 15:10:42 +0200

    add new block generator

--- web/app/templates/modules/lib_interface/blocks.js
+++ web/app/templates/modules/lib_interface/blocks.js
@@ -1,73 +1,108 @@
 Blockly.Blocks['move_forward'] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("Avancer");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
+  init: function() {
+    this.appendValueInput("SPEED")
+        .setCheck("Number")
+        .appendField("Move forward with speed");
+    this.appendValueInput("DURATION")
+        .setCheck("Number")
+        .appendField("for duration (seconds, optional)");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
 };
 
 Blockly.Blocks['move_backward'] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("Reculer");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
+  init: function() {
+    this.appendValueInput("SPEED")
+        .setCheck("Number")
+        .appendField("Move backward with speed");
+    this.appendValueInput("DURATION")
+        .setCheck("Number")
+        .appendField("for duration (seconds, optional)");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
 };
 
+Blockly.Blocks['move'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Move")
+        .appendField(new Blockly.FieldDropdown([["forward", "FORWARD"], ["backward", "BACKWARD"]]), "DIRECTION");
+    this.appendValueInput("SPEED")
+        .setCheck("Number")
+        .appendField("with speed");
+    this.appendValueInput("DURATION")
+        .setCheck("Number")
+        .appendField("for duration (seconds, optional)");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+
 Blockly.Blocks['turn_left'] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("Tourner à gauche");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
+  init: function() {
+    this.appendValueInput("SPEED")
+        .setCheck("Number")
+        .appendField("Turn left with speed");
+    this.appendValueInput("DURATION")
+        .setCheck("Number")
+        .appendField("for duration (seconds, optional)");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
 };
 
 Blockly.Blocks['turn_right'] = {
-    init: function() {
-        this.appendDummyInput()
-            .appendField("Tourner à droite");
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(230);
-        this.setTooltip("");
-        this.setHelpUrl("");
-    }
+  init: function() {
+    this.appendValueInput("SPEED")
+        .setCheck("Number")
+        .appendField("Turn right with speed");
+    this.appendValueInput("DURATION")
+        .setCheck("Number")
+        .appendField("for duration (seconds, optional)");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
 };
-  
-  Blockly.Blocks['stop_robot'] = {
-    init: function() {
-      this.appendDummyInput()
-          .appendField("Stop robot");
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(120);
-      this.setTooltip('');
-      this.setHelpUrl('');
-    }
-  };
-  
-  Blockly.Blocks['sleep'] = {
-    init: function() {
-      this.appendValueInput("DURATION")
-          .setCheck("Number")
-          .appendField("Sleep for duration (seconds)");
-      this.setPreviousStatement(true, null);
-      this.setNextStatement(true, null);
-      this.setColour(120);
-      this.setTooltip('');
-      this.setHelpUrl('');
-    }
-  };
-  
+
+Blockly.Blocks['stop_robot'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Stop robot");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['sleep'] = {
+  init: function() {
+    this.appendValueInput("DURATION")
+        .setCheck("Number")
+        .appendField("Sleep for duration (seconds)");
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(120);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
--- web/app/templates/modules/lib_interface/python_generators.js
+++ web/app/templates/modules/lib_interface/python_generators.js
@@ -1,55 +1,71 @@
-Blockly.Python['move_forward'] = function(block) {
-    var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
-    var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
-    if (value_duration) {
-      var code = `forward(rosa, ${value_speed}, ${value_duration})\n`;
-    } else {
-      var code = `forward(rosa, ${value_speed})\n`;
-    }
-    return code;
-  };
-  
-  Blockly.Python['move_backward'] = function(block) {
-    var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
-    var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
-    if (value_duration) {
-      var code = `backward(rosa, ${value_speed}, ${value_duration})\n`;
-    } else {
-      var code = `backward(rosa, ${value_speed})\n`;
-    }
-    return code;
-  };
-  
-  Blockly.Python['turn_left'] = function(block) {
-    var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
-    var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
-    if (value_duration) {
-      var code = `turn_left(rosa, ${value_speed}, ${value_duration})\n`;
-    } else {
-      var code = `turn_left(rosa, ${value_speed})\n`;
-    }
-    return code;
-  };
-  
-  Blockly.Python['turn_right'] = function(block) {
-    var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
-    var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
-    if (value_duration) {
-      var code = `turn_right(rosa, ${value_speed}, ${value_duration})\n`;
-    } else {
-      var code = `turn_right(rosa, ${value_speed})\n`;
-    }
-    return code;
-  };
-  
-  Blockly.Python['stop_robot'] = function(block) {
-    var code = 'stop(rosa)\n';
-    return code;
-  };
-  
-  Blockly.Python['sleep'] = function(block) {
-    var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
-    var code = `sleep(${value_duration})\n`;
-    return code;
-  };
-  
+python.pythonGenerator.forBlock['move_forward'] = function(block, generator) {
+  var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
+  var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+  if (value_duration) {
+    var code = `forward(rosa, ${value_speed}, ${value_duration})\n`;
+  } else {
+    var code = `forward(rosa, ${value_speed})\n`;
+  }
+  return code;
+};
+
+python.pythonGenerator.forBlock['move'] = function(block, generator) {
+  var dropdown_direction = block.getFieldValue('DIRECTION');
+  var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
+  var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+
+  var directionFunction = dropdown_direction === 'FORWARD' ? 'forward' : 'backward';
+
+  if (value_duration) {
+    var code = `${directionFunction}(rosa, ${value_speed}, ${value_duration})\n`;
+  } else {
+    var code = `${directionFunction}(rosa, ${value_speed})\n`;
+  }
+
+  return code;
+};
+
+
+Blockly.Python['move_backward'] = function(block) {
+  var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
+  var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+  if (value_duration) {
+    var code = `backward(rosa, ${value_speed}, ${value_duration})\n`;
+  } else {
+    var code = `backward(rosa, ${value_speed})\n`;
+  }
+  return code;
+};
+
+Blockly.Python['turn_left'] = function(block) {
+  var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
+  var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+  if (value_duration) {
+    var code = `turn_left(rosa, ${value_speed}, ${value_duration})\n`;
+  } else {
+    var code = `turn_left(rosa, ${value_speed})\n`;
+  }
+  return code;
+};
+
+Blockly.Python['turn_right'] = function(block) {
+  var value_speed = Blockly.Python.valueToCode(block, 'SPEED', Blockly.Python.ORDER_ATOMIC);
+  var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+  if (value_duration) {
+    var code = `turn_right(rosa, ${value_speed}, ${value_duration})\n`;
+  } else {
+    var code = `turn_right(rosa, ${value_speed})\n`;
+  }
+  return code;
+};
+
+Blockly.Python['stop_robot'] = function(block) {
+  var code = `stop(rosa)\n`;
+  return code;
+};
+
+Blockly.Python['sleep'] = function(block) {
+  var value_duration = Blockly.Python.valueToCode(block, 'DURATION', Blockly.Python.ORDER_ATOMIC);
+  var code = `sleep(${value_duration})\n`;
+  return code;
+};
